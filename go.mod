13413b20583f3bacc932ed24da23a112e9d4bf78
Pragmatically-dev <pragmatically.dev@gmail.com>
2025-01-25 23:14:27 -0300

    Sending first email

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/pragmatically-dev/sendgrid-go-test
 
 go 1.23.5
+
+require (
+	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.16.0+incompatible // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	from := mail.NewEmail("NR Entropía", "entropia.talleres.dev@gmail.com")
+	subject := "Este es un test de envio"
+	to := mail.NewEmail("Santiago", "pragmatically.dev@gmail.com")
+	plainTextContent := "Este es un test de envio"
+	htmlContent := "<strong>Test de envio desde golang</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	response, err := client.Send(message)
+	if err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(response.StatusCode)
+
+	}
 }
